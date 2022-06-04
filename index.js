c8d9fe7469612e6791af32348912c38eab38ed47
sriayu <sriaayuwlndr@gmail.com>
2022-06-04 14:39:19 +0700

    Percobaan 2

--- index.js
+++ index.js
@@ -1,67 +1,56 @@
 // ! JANGAN DIMODIFIKASI
 const dates = [
-  "2019-01-02", // 1546387200 (epoch time, dalam detik)
-  "2022-05-06 00:00:00 UTC-2", // 1651802400 (epoch time, dalam detik)
-  "2021-03-04 07:00:00 UTC", // 1614841200 (epoch time, dalam detik)
-  "2021-04-05 00:00:00 UTC+2", // 1617573600 (epoch time, dalam detik)
-  "2020-02-03 UTC+7", // 1580662800 (epoch time, dalam detik)
-];
-
-// console.log(dates);
-
-
-// TODO: Buatlah fungsi createDate
-const createDate = (data,posisi) => {
-
-  let panjangData = data.length;
-  let resultAll = [];
-  let resultBerdasarkanPosisi = [];
+    "2019-01-02", // 1546387200 (epoch time, dalam detik)
+    "2022-05-06 00:00:00 UTC-2", // 1651802400 (epoch time, dalam detik)
+    "2021-03-04 07:00:00 UTC", // 1614841200 (epoch time, dalam detik)
+    "2021-04-05 00:00:00 UTC+2", // 1617573600 (epoch time, dalam detik)
+    "2020-02-03 UTC+7", // 1580662800 (epoch time, dalam detik)
+  ];
   
+  
+  
+  // TODO: Buatlah fungsi createDate
+  const createDate = (data, posisi) => {
+    let panjangData = data.length;
+    let resultAll = [];
+    let result = [];
 
-  //SEMUA DATA
-  for (let i = 0; i < panjangData; i++)
-  {
-    resultAll[i] = Date.parse(data[i]);
-    
-    resultAll[i] = resultAll[i]/1000;
-    
+    if(posisi == undefined)
+    {
+        // return dates[3];
+        for (let i = 0; i < panjangData; i++)
+        {
+            resultAll[i] = Date.parse(data[i]);
 
-    // console.log(resultAll[i]);
+            resultAll[i] = resultAll[i]/1000; //second
+
+            result[i] = resultAll[i];
+        }
+
+        return result.join("-");
+    }
+
+    else
+    {
+        // return data[posisi]/1000;
+
+        return Date.parse(data[posisi])/1000;
+    }
   }
-
-  // for (let i = posisi; i == posisi; i++)
-  // {
-  //   resultBerdasarkanPosisi[i] = Date.parse(data[i]);
-    
-  //   resultBerdasarkanPosisi[i] = resultBerdasarkanPosisi[i]/1000;
-    
-
-  //   // console.log(resultAll[i]);
-  // }
-
-  // DATA BERDASARKAN POSISI
-  // let resultBerdasarkanPosisi = "";
-  resultBerdasarkanPosisi[posisi] = Date.parse(data[posisi]);
-  resultBerdasarkanPosisi[posisi] = resultBerdasarkanPosisi[posisi]/1000;
-// console.log(resultBerdasarkanPosisi[posisi]);
-
-
-
-  return {resultAll, resultBerdasarkanPosisi};
-}
-
-// ! JANGAN DIMODIFIKASI
-(() => {
-  // IIFE
-
-  // '1546387200-1580662800-1614841200-1617573600-1651802400' (dalam string)
-  console.log(createDate?.(dates));
-
-  // '1614841200' (dalam string)
-  console.log(createDate?.(dates, 2));
-})();
-
-module.exports = {
-  dates,
-  createDate,
-};
+  
+  // ! JANGAN DIMODIFIKASI
+  (() => {
+    // IIFE
+  
+    // '1546387200-1580662800-1614841200-1617573600-1651802400' (dalam string)
+    console.log(createDate?.(dates));
+  
+    // '1614841200' (dalam string)
+    console.log(createDate?.(dates, 2));
+  })();
+  
+  module.exports = {
+    dates,
+    createDate,
+  };
+  
